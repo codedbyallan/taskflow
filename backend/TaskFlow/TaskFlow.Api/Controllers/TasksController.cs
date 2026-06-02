944ac9fc15051c2bde2cb92285d20741517ca532
Allan Barbosa <allan.barbosa@yahoo.com.br>
2026-06-01 23:19:21 -0300

    feat: adiciona controller inicial de tarefas

---
+++ backend/TaskFlow/TaskFlow.Api/Controllers/TasksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetTasks()
+        {
+            return Ok("Tasks endpoint is working");
+        }
+    }
+}
