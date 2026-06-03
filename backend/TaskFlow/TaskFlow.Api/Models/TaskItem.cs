696f4bda318028cffaa8f51e6a9f56e6946df996
Allan Barbosa <allan.barbosa@yahoo.com.br>
2026-06-03 19:50:31 -0300

    refactor: adapta model de tarefa para mongodb

--- backend/TaskFlow/TaskFlow.Api/Models/TaskItem.cs
+++ backend/TaskFlow/TaskFlow.Api/Models/TaskItem.cs
@@ -1,7 +1,12 @@
-﻿namespace TaskFlow.Api.Models
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TaskFlow.Api.Models
 {
     public class TaskItem
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; } 
         public string Title { get; set; } = string.Empty;
         public string? Description { get; set; }
