425e17e01b1d9b896e248b33bc8484a46d1dcda0
Allan Barbosa <allan.barbosa@yahoo.com.br>
2026-06-03 18:29:33 -0300

    chore: adiciona settings do mongodb

--- backend/TaskFlow/TaskFlow.Api/Settings/MongoDbSettings.cs
+++ backend/TaskFlow/TaskFlow.Api/Settings/MongoDbSettings.cs
@@ -2,5 +2,8 @@
 {
     public class MongoDbSettings
     {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string DatabaseName { get; set; } = string.Empty;
+        public string TasksCollectionName { get; set; } = string.Empty;
     }
 }
